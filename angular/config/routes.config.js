4e48cb3bee905c6a00457039543fa1dc940420c8
Mark Heramis <chumheramis@gmail.com>
2018-06-12 19:50:39 +0800

    prettify routes.

--- angular/config/routes.config.js
+++ angular/config/routes.config.js
@@ -1,25 +1,44 @@
-export function RoutesConfig ($stateProvider, $urlRouterProvider) {
-  'ngInject'
+export function RoutesConfig($stateProvider, $urlRouterProvider) {
+    'ngInject'
 
-  var getView = (viewName) => {
-    return `./views/app/pages/${viewName}/${viewName}.page.html`
-  }
+    var getView = (viewName) => {
+        return `./views/app/pages/${viewName}/${viewName}.page.html`
+    }
 
-  var getLayout = (layout) => {
-    return `./views/app/pages/layout/${layout}.page.html`
-  }
+    var getLayout = (layout) => {
+        return `./views/app/pages/layout/${layout}.page.html`
+    }
 
-  $urlRouterProvider.otherwise('/')
+    $urlRouterProvider.otherwise('/')
 
-  $stateProvider
+    $stateProvider
     .state('app', {
-      abstract: true,
-      views: {
-        'layout': {
-          templateUrl: getLayout('layout')
+        abstract: true,
+        views: {
+            'layout': {
+                templateUrl: getLayout('layout')
+            },
+            'header@app': {
+                templateUrl: getView('header')
+            },
+            'footer@app': {
+                templateUrl: getView('footer')
+            },
+            main: {}
         },
-        'header@app': {
-          templateUrl: getView('header')
+        data: {
+            bodyClass: 'hold-transition skin-blue sidebar-mini'
+        }
+    })
+    .state('app.landing', {
+        url: '/',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                templateUrl: getView('landing')
+            }
+        }
+    })
     .state('app.sales-create',{
         url: '/sales/create',
         data: {auth: true},
@@ -28,8 +47,6 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
                 template:'<sales-create></sales-create>'
             }
         },
-        'footer@app': {
-          templateUrl: getView('footer')
         params: {
             alerts: null
         }
@@ -51,24 +68,12 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
                 template: '<sales-edit></sales-edit>'
             }
         },
-        main: {}
-      },
-      data: {
-        bodyClass: 'hold-transition skin-blue sidebar-mini'
-      }
         params:{
             alerts: null,
             salesId: null
         }
     })
-    .state('app.landing', {
-      url: '/',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          templateUrl: getView('landing')
+    /*
     .state('app.sales',{
         url: '/transactions/sales',
         data: {auth: true},
@@ -81,20 +86,16 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
             alerts: null
         }
     })
-    })
-    .state('app.profile', {
-      url: '/profile',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-profile></user-profile>'
+    .state('app.receivings',{
+        url: '/transactions/receivings',
+        data: {auth: true},
+        views: {
+            'main@app':{
+                template: '<receivings></receivings>'
+            }
         }
-      },
-      params: {
-        alerts: null
-      }
+    })
+    */
     .state('app.item-kits-list',{
         url: '/item/kits/list',
         data: {auth: true},
@@ -104,14 +105,6 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
             }
         }
     })
-    .state('app.userlist', {
-      url: '/user-lists',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-lists></user-lists>'
     .state('app.item-kits-add',{
         url: '/item/kits/add',
         data: {auth: true},
@@ -171,20 +164,6 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
             itemId: null
         }
     })
-    .state('app.useredit', {
-      url: '/user-edit/:userId',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-edit></user-edit>'
-        }
-      },
-      params: {
-        alerts: null,
-        userId: null
-      }
     .state('app.supplier-list',{
         url: '/supplier/list',
         data: {auth: true},
@@ -219,14 +198,6 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
             supplierId: null
         }
     })
-    .state('app.userroles', {
-      url: '/user-roles',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-roles></user-roles>'
     .state('app.customer-list',{
         url: '/customer/list',
         data: {auth: true},
@@ -262,181 +233,216 @@ export function RoutesConfig ($stateProvider, $urlRouterProvider) {
         }
     })
     .state('app.userpermissions', {
-      url: '/user-permissions',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-permissions></user-permissions>'
+        url: '/user-permissions',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-permissions></user-permissions>'
+            }
         }
-      }
-    })
-    .state('app.userpermissionsadd', {
-      url: '/user-permissions-add',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-permissions-add></user-permissions-add>'
-        }
-      },
-      params: {
-        alerts: null
-      }
     })
     .state('app.userpermissionsedit', {
-      url: '/user-permissions-edit/:permissionId',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-permissions-edit></user-permissions-edit>'
+        url: '/user-permissions/edit/:permissionId',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-permissions-edit></user-permissions-edit>'
+            }
+        },
+        params: {
+            alerts: null,
+            permissionId: null
         }
-      },
-      params: {
-        alerts: null,
-        permissionId: null
-      }
     })
-    .state('app.userrolesadd', {
-      url: '/user-roles-add',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-roles-add></user-roles-add>'
+    .state('app.userpermissionsadd', {
+        url: '/user-permissions/add',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-permissions-add></user-permissions-add>'
+            }
+        },
+        params: {
+            alerts: null
         }
-      },
-      params: {
-        alerts: null
-      }
+    })
+
+    .state('app.userroles', {
+        url: '/user-roles',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-roles></user-roles>'
+            }
+        }
     })
     .state('app.userrolesedit', {
-      url: '/user-roles-edit/:roleId',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<user-roles-edit></user-roles-edit>'
+        url: '/user-roles/edit/:roleId',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-roles-edit></user-roles-edit>'
+            }
+        },
+        params: {
+            alerts: null,
+            roleId: null
         }
-      },
-      params: {
-        alerts: null,
-        roleId: null
-      }
     })
-    .state('app.widgets', {
-      url: '/widgets',
-      data: {
-        auth: true
-      },
-      views: {
-        'main@app': {
-          template: '<widgets></widgets>'
+
+    .state('app.userrolesadd', {
+        url: '/user-roles/add',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-roles-add></user-roles-add>'
+            }
+        },
+        params: {
+            alerts: null
         }
-      }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-profile></user-profile>'
+            }
+        },
+        params: {
+            alerts: null
+        }
+    })
+    .state('app.userlist', {
+        url: '/user/lists',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-lists></user-lists>'
+            }
+        }
+    })
+    .state('app.useredit', {
+        url: '/user/edit/:userId',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-edit></user-edit>'
+            }
+        },
+        params: {
+            alerts: null,
+            userId: null
+        }
+    })
+    .state('app.useradd',{
+        url: '/user/add',
+        data: {auth: true},
+        views: {
+            'main@app': {
+                template: '<user-add></user-add>'
+            }
+        }
+    })
+
+    .state('loginloader', {
+        url: '/login-loader',
+        views: {
+            'layout': {
+                templateUrl: getView('login-loader')
+            },
+            'header@app': {},
+            'footer@app': {}
+        },
+        data: {
+            bodyClass: 'hold-transition login-page'
+        }
     })
     .state('login', {
-      url: '/login',
-      views: {
-        'layout': {
-          templateUrl: getView('login')
+        url: '/login',
+        views: {
+            'layout': {
+                templateUrl: getView('login')
+            },
+            'header@app': {},
+            'footer@app': {}
         },
-        'header@app': {},
-        'footer@app': {}
-      },
-      data: {
-        bodyClass: 'hold-transition login-page'
-      },
-      params: {
-        registerSuccess: null,
-        successMsg: null
-      }
-    })
-    .state('loginloader', {
-      url: '/login-loader',
-      views: {
-        'layout': {
-          templateUrl: getView('login-loader')
+        data: {
+            bodyClass: 'hold-transition login-page'
         },
-        'header@app': {},
-        'footer@app': {}
-      },
-      data: {
-        bodyClass: 'hold-transition login-page'
-      }
+        params: {
+            registerSuccess: null,
+            successMsg: null
+        }
     })
     .state('register', {
-      url: '/register',
-      views: {
-        'layout': {
-          templateUrl: getView('register')
+        url: '/register',
+        views: {
+            'layout': {
+                templateUrl: getView('register')
+            },
+            'header@app': {},
+            'footer@app': {}
         },
-        'header@app': {},
-        'footer@app': {}
-      },
-      data: {
-        bodyClass: 'hold-transition register-page'
-      }
-    })
-    .state('userverification', {
-      url: '/userverification/:status',
-      views: {
-        'layout': {
-          templateUrl: getView('user-verification')
+        data: {
+            bodyClass: 'hold-transition register-page'
         }
-      },
-      data: {
-        bodyClass: 'hold-transition login-page'
-      },
-      params: {
-        status: null
-      }
     })
     .state('forgot_password', {
-      url: '/forgot-password',
-      views: {
-        'layout': {
-          templateUrl: getView('forgot-password')
+        url: '/forgot-password',
+        views: {
+            'layout': {
+                templateUrl: getView('forgot-password')
+            },
+            'header@app': {},
+            'footer@app': {}
         },
-        'header@app': {},
-        'footer@app': {}
-      },
-      data: {
-        bodyClass: 'hold-transition login-page'
-      }
+        data: {
+            bodyClass: 'hold-transition login-page'
+        }
     })
     .state('reset_password', {
-      url: '/reset-password/:email/:token',
-      views: {
-        'layout': {
-          templateUrl: getView('reset-password')
+        url: '/reset-password/:email/:token',
+        views: {
+            'layout': {
+                templateUrl: getView('reset-password')
+            },
+            'header@app': {},
+            'footer@app': {}
         },
-        'header@app': {},
-        'footer@app': {}
-      },
-      data: {
-        bodyClass: 'hold-transition login-page'
-      }
+        data: {
+            bodyClass: 'hold-transition login-page'
+        }
+    })
+    .state('userverification', {
+        url: '/userverification/:status',
+        views: {
+            'layout': {
+                templateUrl: getView('user-verification')
+            }
+        },
+        data: {
+            bodyClass: 'hold-transition login-page'
+        },
+        params: {
+            status: null
+        }
     })
     .state('app.logout', {
-      url: '/logout',
-      views: {
-        'main@app': {
-          controller: function ($rootScope, $scope, $auth, $state, AclService) {
-            $auth.logout().then(function () {
-              delete $rootScope.me
-              AclService.flushRoles()
-              AclService.setAbilities({})
-              $state.go('login')
-            })
-          }
+        url: '/logout',
+        views: {
+            'main@app': {
+                controller: function ($rootScope, $scope, $auth, $state, AclService) {
+                    $auth.logout().then(function () {
+                        delete $rootScope.me
+                        AclService.flushRoles()
+                        AclService.setAbilities({})
+                        $state.go('login')
+                    })
+                }
+            }
         }
-      }
     })
 }
