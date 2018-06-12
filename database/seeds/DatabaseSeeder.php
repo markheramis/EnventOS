e4e2c249ca55a4310deb719f47e990a5e3402ac0
Mark Heramis <chumheramis@gmail.com>
2018-06-12 19:53:26 +0800

    call seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,24 +3,23 @@
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         Model::unguard();
-
         $this->call(UsersTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(RoleUserTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-
+        $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(CustomerTableSeeder::class);
         $this->call(SuppliersTableSeeder::class);
+        $this->call(ItemsSeeder::class);
+        $this->call(InventoriesTableSeeder::class);
+        $this->call(ItemKitsSeeder::class);
         Model::reguard();
     }
+
 }
