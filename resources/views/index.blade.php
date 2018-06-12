5caf5a8e67477ec534210a32446379f5921bfd37
Mark Heramis <chumheramis@gmail.com>
2018-06-12 19:51:01 +0800

    use localized font-awesome.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="{!! elixir('css/final.css') !!}">
     <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic'>
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <title>Laravel Angular Admin-LTE</title>
