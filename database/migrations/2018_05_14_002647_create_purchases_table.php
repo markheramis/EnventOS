e2c4b47a4790ac6011603b50eedc2fccc5e0df9b
Mark Heramis <chumheramis@gmail.com>
2018-07-11 15:49:16 +0800

    remove columns I don't really need.

--- database/migrations/2018_05_14_002647_create_purchases_table.php
+++ database/migrations/2018_05_14_002647_create_purchases_table.php
@@ -19,8 +19,6 @@ class CreatePurchasesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->string('payment_type',15)->nullable();
             $table->decimal('cost_price',9,2);
-            $table->decimal('selling_price',9,2);
-            $table->decimal('amount_tendered',9,2);
             $table->string('comments',255)->default('N/A')->nullable();
             $table->timestamps();
 
--- database/migrations/2018_05_14_165451_create_purchase_items_table.php
+++ database/migrations/2018_05_14_165451_create_purchase_items_table.php
@@ -17,7 +17,6 @@ class CreatePurchaseItemsTable extends Migration
             $table->integer('purchase_id')->unsigned();
             $table->integer('item_id')->unsigned();
             $table->decimal('cost_price',9,2);
-            $table->decimal('selling_price',9,2);
             $table->integer('quantity');
             $table->decimal('total_cost',9,2);
             $table->timestamps();
