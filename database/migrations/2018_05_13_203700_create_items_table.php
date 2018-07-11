9e47fde8c7a206e88756e3ff98c8418abe708b4e
Mark Heramis <chumheramis@gmail.com>
2018-07-11 15:49:43 +0800

    remove item type

--- database/migrations/2018_05_13_203700_create_items_table.php
+++ database/migrations/2018_05_13_203700_create_items_table.php
@@ -22,7 +22,6 @@ class CreateItemsTable extends Migration
 			$table->string('avatar', 255)->default('no-foto.png');
 			$table->decimal('cost_price',9, 2);
 			$table->decimal('selling_price',9, 2);
-			$table->integer('type')->default(1);
 			$table->timestamps();
 		});
     }
