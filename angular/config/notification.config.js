f0c72d2ed3164afa360758b198d2137aabb67c52
Mark Heramis <chumheramis@gmail.com>
2018-07-18 15:54:44 +0800

    add angular-notification library
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

---
+++ angular/config/notification.config.js
@@ -0,0 +1,11 @@
+export function NotificationConfig (NotificationProvider) {
+    NotificationProvider.setOptions({
+        delay: 10000,
+        startTop: 80,
+        startRight: 10,
+        verticalSpacing: 20,
+        horizontalSpacing: 20,
+        positionX: 'right',
+        positionY: 'top'
+    })
+}
--- angular/index.config.js
+++ angular/index.config.js
@@ -2,9 +2,11 @@ import { AclConfig } from './config/acl.config'
 import { RoutesConfig } from './config/routes.config'
 import { LoadingBarConfig } from './config/loading_bar.config'
 import { SatellizerConfig } from './config/satellizer.config'
+import { NotificationConfig } from './config/notification.config'
 
 angular.module('app.config')
 .config(AclConfig)
 .config(RoutesConfig)
 .config(LoadingBarConfig)
 .config(SatellizerConfig)
+.config(NotificationConfig)
