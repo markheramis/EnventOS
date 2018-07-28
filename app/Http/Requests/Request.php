8205386790fffff54b92a8c3fc6654fbf0bf48f0
Mark Heramis <chumheramis@gmail.com>
2018-07-29 01:11:16 +0800

    add constructor and default rules function
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Requests;
 
+use Auth;
 use Dingo\Api\Http\FormRequest;
-use Illuminate\Http\JsonResponse;
+
 abstract class Request extends FormRequest
 {
-    //
+	protected $user = null;
+    public function __construct(){
+    	$this->user = Auth::user();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            
+        ];
+    }
 }
